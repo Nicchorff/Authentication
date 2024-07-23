26d804badc36fa70d95a57ec0089ed246445ad28
Pedro Henrique Nicchio Kerkoff <160201116+Nicchorff@users.noreply.github.com>
2024-07-23 15:25:22 -0300

    Add project files.

---
+++ Authentication/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly HttpContent _httpContent;
+        private readonly IDataProtectionProvider _dataProtectionProvider;
+
+        public UserController(HttpContent httpContent, IDataProtectionProvider dataProtectionProvider)
+        {
+            _httpContent = httpContent;
+            _dataProtectionProvider = dataProtectionProvider;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
